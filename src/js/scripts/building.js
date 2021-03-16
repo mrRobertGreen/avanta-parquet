f2c69b1938ec67c1f80c1e395fe10497d9a1705b
Robert Green <mrrobertgreen17@gmail.com>
2021-03-16 18:07:47 +0300

    work

--- src/js/scripts/building.js
+++ src/js/scripts/building.js
@@ -15,3 +15,18 @@ $(".material__item").on("click", (e) => {
 chahgeSubmitButtonText()
 
 $(window).resize(chahgeSubmitButtonText)
+
+
+$(window).on("scroll", function () {
+    // sticky-эффект у выбранной текстуры
+    const menuOffset = document.querySelector("#building_board .chosen").offsetTop + window.innerHeight
+    const finalBlockOffset = document.querySelector("#building_hungarian").offsetTop + window.innerHeight
+    const fakeMenuOffset = document.querySelector(".menu_fake").offsetTop + window.innerHeight
+    const scrollTop = $(window).scrollTop()
+    if (scrollTop >= menuOffset - 145 && scrollTop <= finalBlockOffset) {
+        $("#building_board .chosen").addClass('_fixed');
+    }
+    if (scrollTop < menuOffset || scrollTop > finalBlockOffset) {
+        $("#building_board .chosen").removeClass('_fixed');
+    }
+})
