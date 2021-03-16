3e8153cd9d512d2a07318f508afebef9af7155fc
Robert Green <mrrobertgreen17@gmail.com>
2021-03-16 21:51:05 +0300

    work

--- src/js/scripts/building.js
+++ src/js/scripts/building.js
@@ -19,14 +19,21 @@ $(window).resize(chahgeSubmitButtonText)
 
 $(window).on("scroll", function () {
     // sticky-эффект у выбранной текстуры
-    const menuOffset = document.querySelector("#building_board .chosen").offsetTop + window.innerHeight
-    const finalBlockOffset = document.querySelector("#building_hungarian").offsetTop + window.innerHeight
-    const fakeMenuOffset = document.querySelector(".menu_fake").offsetTop + window.innerHeight
+    if (window.matchMedia("(max-width: 1000px)").matches) return
+    const marginTopStart = 75
+    const marginBottomFinish = 45
+    const start = document.querySelector("#building_board .texture__title").offsetTop + window.innerHeight - marginTopStart
+    const finish = document.querySelector("#building_board .calc-wrapper").offsetTop + window.innerHeight - marginBottomFinish
     const scrollTop = $(window).scrollTop()
-    if (scrollTop >= menuOffset - 145 && scrollTop <= finalBlockOffset) {
-        $("#building_board .chosen").addClass('_fixed');
+    if (scrollTop >= start && scrollTop <= finish) {
+        $("#building_board .chosen").addClass("_fixed");
+        $("#building_board .chosen_down").addClass("_hidden")
     }
-    if (scrollTop < menuOffset || scrollTop > finalBlockOffset) {
-        $("#building_board .chosen").removeClass('_fixed');
+    if (scrollTop < start) {
+        $("#building_board .chosen").removeClass("_fixed");
+    }
+    if (scrollTop > finish) {
+        $("#building_board .chosen").removeClass("_fixed");
+        $("#building_board .chosen_down").removeClass("_hidden")
     }
 })
