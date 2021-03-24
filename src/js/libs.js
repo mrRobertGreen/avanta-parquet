8b57fad74bb718bff603c5f7ca9e5ed7fbb3eba5
Robert Green <mrrobertgreen17@gmail.com>
2021-03-24 20:38:48 +0300

    work

--- src/js/libs.js
+++ src/js/libs.js
@@ -3,3 +3,4 @@
 @@include("./libs/sly.js")
 @@include("./libs/slick.min.js")
 @@include("./libs/webkit-input-range-fill-lower.min.js")
+@@include("./libs/lazysizes.min.js")
--- src/js/scripts/building.js
+++ src/js/scripts/building.js
@@ -16,7 +16,6 @@ chahgeSubmitButtonText()
 
 $(window).resize(chahgeSubmitButtonText)
 
-
 $(window).on("scroll", function () {
     stickyChosenImageFlow("building_board")
     stickyChosenImageFlow("building_hungarian")
@@ -41,3 +40,16 @@ const stickyChosenImageFlow = (productId) => {  // sticky-эффект у выб
         $(`#${productId} .chosen_down`).removeClass("_hidden")
     }
 }
+
+// lazy loading
+(async () => {
+    if ('loading' in HTMLImageElement.prototype) {
+        const images = document.querySelectorAll("img.lazyload");
+        images.forEach(img => {
+            img.src = img.dataset.src;
+        });
+    } else {
+        // Инициализация LazySizes (чтение data-src & class=lazyload)
+        lazySizes.init(); // lazySizes применяется при обработке изображений, находящихся на странице.
+    }
+})();
